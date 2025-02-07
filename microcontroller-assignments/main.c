07d37e6e1ac27a6565db74e626ca6f7ac7873cdd
tiemenbr <tiemenbr@hotmail.nl>
2025-02-07 14:38:55 +0100

    JTAG

--- microcontroller-assignments/main.c
+++ microcontroller-assignments/main.c
@@ -2,17 +2,32 @@
  * microcontroller-assignments.c
  *
  * Created: 7-2-2025 14:09:34
- * Author : tieme
+ * Author : Tiemen and Tom
  */ 
 
-#include <avr/io.h>
+#define F_CPU 8000000UL
 
+#include <avr/io.h>
+#include <util/delay.h>
+
+void wait(int ms) {
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms(1);
+	}
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRA = 0xFF;
+	
+	while (1)
+	{
+		PORTA = 0xF0;
+		wait(1000);
+		PORTA = 0x0F;
+		wait(1000);
+	}
 }
+
 
