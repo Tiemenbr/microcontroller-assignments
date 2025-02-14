0ccaf20e79882b25ef5bbd00cadd0287494cabcb
Tom <tom.martens01@gmail.com>
2025-02-14 10:23:52 +0100

    W2B1 mid progress

---
+++ week2assignmentB1/week2assignmentB1/main.c
@@ -0,0 +1,18 @@
+/*
+ * week2assignmentB1.c
+ *
+ * Created: 14-2-2025 10:19:44
+ * Author : tcpma
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
