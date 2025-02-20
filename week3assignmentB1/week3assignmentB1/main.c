a2a0cbaaa0757a6a31ff07140358f4c1021e68fc
tiemenbr <tiemenbr@hotmail.nl>
2025-02-20 11:53:54 +0100

    B1 counter made

--- week3assignmentB1/week3assignmentB1/main.c
+++ week3assignmentB1/week3assignmentB1/main.c
@@ -1,18 +1,30 @@
 /*
- * week3assignmentB1.c
+ * counter_t2.c
  *
- * Created: 20-2-2025 11:04:18
+ * Created: 21/02/2021 12:45:33
  * Author : tieme
  */ 
 
+#define F_CPU 8e6
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
-
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+void wait( int ms ) {
+	for (int tms=0; tms<ms; tms++) {
+		_delay_ms( 1 );	
+	}
 }
 
+
+int main(void) {
+    
+	DDRD &= ~(1 << (7));
+	DDRA = 0xFF;
+	TCCR2 = 0b00000111;
+
+    while (1) {
+		PORTA = TCNT2;
+		wait(10);
+    }
+}
