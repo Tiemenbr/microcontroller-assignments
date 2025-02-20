925592c6f32d7b60aa713289d56e28d7ca3b1ebb
tiemenbr <tiemenbr@hotmail.nl>
2025-02-20 11:04:34 +0100

    B1 created

---
+++ week3assignmentB1/week3assignmentB1/main.c
@@ -0,0 +1,18 @@
+/*
+ * week3assignmentB1.c
+ *
+ * Created: 20-2-2025 11:04:18
+ * Author : tieme
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
